8a59de77e21c91451736fc4a8f6053dc6c561b93
1amageek <tmy0x3@icloud.com>
2026-05-22 21:42:15 +0900

    Cache SwiftPM artifacts in CI

--- .github/scripts/build-docc.sh
+++ .github/scripts/build-docc.sh
@@ -3,5 +3,6 @@
 export LC_CTYPE=en_US.UTF-8
 
 set -o pipefail && xcodebuild clean docbuild -scheme CodeEditLanguages \
+    -derivedDataPath ".build" \
     -destination generic/platform=macos \
     OTHER_DOCC_FLAGS="--transform-for-static-hosting --hosting-base-path CodeEditLanguages --output-path ./docs" | xcpretty
